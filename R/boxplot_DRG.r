8a7ff1b9ef49a0753acf13dcdfa8215d2f0b1f4b
Yunqing Ma <yum4004@med.cornell.edu>
2020-10-23 14:07:01 +0800

    file for lab2 package

--- R/boxplot_DRG.r
+++ R/boxplot_DRG.r
@@ -7,29 +7,66 @@
 #'
 #' @examples boxplot_DRG(data = 'total')
 #'
+# Define the function
 boxplot_DRG <- function(data = 'medicare') {
+    # Read data
+    DRG_data <- read.csv('DRG_data.csv')
+
+    # Set x axis text
+    distinct_def <- dplyr::distinct(DRG_data, DRG.Definition)
+    def_split <- strsplit(distinct_def[[1]], ' ')
+    def_id <- c()
+    # Use only the number to indicate the definition
+    for (i in 1:100) {
+        def_id <- c(def_id, def_split[[i]][1])
+    }
+
+    # Select data for plot
     if (data == 'medicare') {
-        boxplot(
-            DRG_data$`Average Medicare Payments`,
-            xlab = 'Medicare',
-            ylab = 'Average Payments',
-            main = 'Average Medicare Payments'
-        )
+        ggplot2::ggplot(DRG_data,
+                        ggplot2::aes(x = DRG.Definition,
+                                     y = Average.Medicare.Payments)) +
+            ggplot2::geom_boxplot() +
+            ggplot2::labs(title = 'Average Medicare Payments',
+                          x = 'Medicare',
+                          y = 'Average Payments') +
+            ggplot2::scale_x_discrete(labels = def_id) +
+            ggplot2::theme(axis.text.x = ggplot2::element_text(
+                angle = 90,
+                hjust = 1,
+                size = 8
+            ))
     }
+    # Select data for plot
     else if (data == 'total') {
-        boxplot(
-            DRG_data$`Average Total Payments`,
-            xlab = 'Total',
-            ylab = 'Average Payments',
-            main = 'Average Total Payments'
-        )
+        ggplot2::ggplot(DRG_data,
+                        ggplot2::aes(x = DRG.Definition,
+                                     y = Average.Total.Payments)) +
+            ggplot2::geom_boxplot() +
+            ggplot2::labs(title = 'Average Total Payments',
+                          x = 'Total',
+                          y = 'Average Payments') +
+            ggplot2::scale_x_discrete(labels = def_id) +
+            ggplot2::theme(axis.text.x = ggplot2::element_text(
+                angle = 90,
+                hjust = 1,
+                size = 8
+            ))
     }
+    # Select data for plot
     else if (data == 'covered') {
-        boxplot(
-            DRG_data$`Average Covered Charges`,
-            xlab = 'Covered',
-            ylab = 'Average Payments',
-            main = 'Average Covered Payments'
-        )
+        ggplot2::ggplot(DRG_data,
+                        ggplot2::aes(x = DRG.Definition,
+                                     y = Average.Covered.Charges)) +
+            ggplot2::geom_boxplot() +
+            ggplot2::labs(title = 'Average Covered Charges',
+                          x = 'Covered',
+                          y = 'Average Charges') +
+            ggplot2::scale_x_discrete(labels = def_id) +
+            ggplot2::theme(axis.text.x = ggplot2::element_text(
+                angle = 90,
+                hjust = 1,
+                size = 8
+            ))
     }
 }
--- R/statistics_DRG_Med.r
+++ R/statistics_DRG_Med.r
@@ -7,17 +7,24 @@
 #'
 #' @examples statistics_DRG_Med(statistics = 'median')
 #'
+# Define the function
 statistics_DRG_Med <- function(statistics = 'mean') {
+    # Read data
+    DRG_data <- read.csv('DRG_data.csv')
+
+    # Select what statistics to calculate
     if (statistics == 'mean') {
-        mean <- mean(DRG_data$`Average Medicare Payments`)
+        mean <- mean(DRG_data$Average.Medicare.Payments)
         return(mean)
     }
-    if (statistics == 'median') {
-        median <- median(DRG_data$`Average Medicare Payments`)
+    # Select what statistics to calculate
+    else if (statistics == 'median') {
+        median <- median(DRG_data$Average.Medicare.Payments)
         return(median)
     }
-    if (statistics == 'sd') {
-        sd <- sd(DRG_data$`Average Medicare Payments`)
+    # Select what statistics to calculate
+    else if (statistics == 'sd') {
+        sd <- sd(DRG_data$Average.Medicare.Payments)
         return(sd)
     }
 }
